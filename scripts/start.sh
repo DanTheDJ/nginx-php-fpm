38a107af05e7cf0edb5cbdc844e34b28610f90e9
Dan Coleman <admin@djmanager.co.uk>
2018-02-20 21:19:25 +0000

    Remove unneeded files

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,39 +20,6 @@ if [[ $lastlinephpconf == *"php_flag[display_errors]"* ]]; then
  sed -i '$ d' /usr/local/etc/php-fpm.conf
 fi
 
-# Display PHP error's or not
-if [[ "$ERRORS" != "1" ]] ; then
- echo php_flag[display_errors] = off >> /usr/local/etc/php-fpm.conf
-else
- echo php_flag[display_errors] = on >> /usr/local/etc/php-fpm.conf
-fi
-
-# Display Version Details or not
-if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
- sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
-else
- sed -i "s/expose_php = On/expose_php = Off/g" /usr/local/etc/php-fpm.conf
-fi
-
-# Pass real-ip to logs when behind ELB, etc
-if [[ "$REAL_IP_HEADER" == "1" ]] ; then
- sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/sites-available/default.conf
- sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/sites-available/default.conf
- if [ ! -z "$REAL_IP_FROM" ]; then
-  sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/sites-available/default.conf
- fi
-fi
-# Do the same for SSL sites
-if [ -f /etc/nginx/sites-available/default-ssl.conf ]; then
- if [[ "$REAL_IP_HEADER" == "1" ]] ; then
-  sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/sites-available/default-ssl.conf
-  sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/sites-available/default-ssl.conf
-  if [ ! -z "$REAL_IP_FROM" ]; then
-   sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/sites-available/default-ssl.conf
-  fi
- fi
-fi
-
 #Display errors in docker logs
 if [ ! -z "$PHP_ERRORS_STDERR" ]; then
   echo "log_errors = On" >> /usr/local/etc/php/conf.d/docker-vars.ini
